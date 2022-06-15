451cf0c883eb5fa7440cc1b7f712fb95de334ad6
Alexander Avdeev <mail@alexanderavdeev.com>
2022-06-15 12:17:31 +0300

    21. 5

--- practice/21.ci-cd/3.prepare_cluster/docker_pull_secret.sh
+++ practice/21.ci-cd/3.prepare_cluster/docker_pull_secret.sh
@@ -6,7 +6,7 @@ kubectl delete secret xpaste-gitlab-registry --namespace "$NS"
 
 kubectl create secret docker-registry xpaste-gitlab-registry \
   --docker-server registry.gitlab.com \
-  --docker-email 'student@slurm.io' \
-  --docker-username '<первая строчка из окна создания токена в gitlab>' \
-  --docker-password '<вторая строчка из окна создания токена в gitlab>' \
+  --docker-email 'mail@alexanderavdeev.com' \
+  --docker-username 'gitlab+deploy-token-1126714' \
+  --docker-password 'csY-AocV3N1x8CeizXgQ' \
   --namespace "$NS"
